bb367b17747e1898b97a943e02e9a0bfda92520a
Carolyn Shin <carolynshin@github.com>
2017-04-22 17:39:53 -0700

    final edits to challenge 1

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,24 @@
 var mysql = require("mysql");
 var inquirer = require('inquirer');
+var Table = require('cli-table');
+
+
+
+
+
+
+
+var table = new Table({
+    head: ['ID', 'Product', 'Department', 'Price', 'Qty']
+  , colWidths: [15, 15, 15, 15, 15]
+});
+
+// table.push(["","","","",""])
+
+// console.log(table.toString());
+
+
+
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -13,61 +32,65 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-});
 
-connection.query("SELECT * FROM products", function(err,res){
-	if (err) throw err;
-	console.log("\n ITEMS FOR SALE \n" + "============================ \n" + JSON.stringify(res) + "\n ============================ \n");
-})
 
-//prompt users with two questions
-var questions = [
-  //QUESTION: what is the ID of the product they would like to buy
-  {
-    type: 'input',
-    name: 'id',
-    message: 'What\'s the ID of the product you would like to buy?'
-  },
-  //QUESITON: what is the number of units they would like to buy
-  {
-    type: 'input',
-    name: 'units',
-    message: 'How many units of the product would you like to buy?',
-    default: function () {
-      return '1';
-    }
-  }
-];
-
-inquirer.prompt(questions).then(function (answers) {
-  //ANSWER: numer of products they would like to buy
-  var requestedQuantity = answers.units;
-
-  //ANSWER: query based on ID of the product they want to buy
-  //Saving stock_quantity of product
-  connection.query("SELECT stock_quantity FROM products WHERE item_id=" + answers.id, function(err, res){
+  connection.query("SELECT * FROM products", function(err,res){
     if (err) throw err;
-    var stockQuantity = res[0].stock_quantity;
-    var balance = stockQuantity-requestedQuantity;
-    console.log("stock quantity is: " + stockQuantity);
-    console.log("requested quantity is: " + requestedQuantity);
 
-    //if the current stock quantity subtracted by the requested quanity is less than zero, end the transaction.
-    if (stockQuantity-requestedQuantity < 0){
-      console.log("Insufficient quantity!");
+    for (var i = 0; i < res.length; i++) {
+      var row = res[i];
+      table.push([row.item_id, row.product_name, row.department_name, row.price, row.stock_quantity]);
     }
-    //otherwise, update the stock quantity to new balance
-    else {
-      connection.query('UPDATE products SET ? WHERE ?', [{ stock_quantity: balance }, { item_id: answers.id }]);
-      connection.query("SELECT price FROM products where item_id=" + answers.id, function(err, res){
-        //
-        var totalCost = res[0].price * requestedQuantity;
+    console.log(table.toString());
+      //prompt users with two questions
+    var questions = [
+      //QUESTION: what is the ID of the product they would like to buy
+      {
+        type: 'input',
+        name: 'id',
+        message: 'What\'s the ID of the product you would like to buy?'
+      },
+      //QUESITON: what is the number of units they would like to buy
+      {
+        type: 'input',
+        name: 'units',
+        message: 'How many units of the product would you like to buy?',
+        default: function () {
+          return '1';
+        }
+      }
+    ];
+
+    inquirer.prompt(questions).then(function (answers) {
+      //ANSWER: numer of products they would like to buy
+      var requestedQuantity = answers.units;
+
+      //ANSWER: query based on ID of the product they want to buy
+      //Saving stock_quantity of product
+      connection.query("SELECT stock_quantity FROM products WHERE item_id=" + answers.id, function(err, res){
         if (err) throw err;
-        console.log("$" + totalCost + " transaction completed. Thank you for your business.");
+        var stockQuantity = res[0].stock_quantity;
+        var balance = stockQuantity-requestedQuantity;
+        console.log("stock quantity is: " + stockQuantity);
+        console.log("requested quantity is: " + requestedQuantity);
+
+        //if the current stock quantity subtracted by the requested quanity is less than zero, end the transaction.
+        if (stockQuantity-requestedQuantity < 0){
+          console.log("Insufficient quantity!");
+        }
+        //otherwise, update the stock quantity to new balance
+        else {
+          connection.query('UPDATE products SET ? WHERE ?', [{ stock_quantity: balance }, { item_id: answers.id }]);
+          connection.query("SELECT price FROM products where item_id=" + answers.id, function(err, res){
+            //
+            var totalCost = res[0].price * requestedQuantity;
+            if (err) throw err;
+            console.log("$" + totalCost + " transaction completed. Thank you for your business.");
+          })
+
+        }
       })
-
-    }
-
+    });
   })
 });
 
